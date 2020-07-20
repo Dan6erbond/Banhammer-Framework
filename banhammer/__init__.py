2884b27c2af095aaf4ea801f7a7177b2b3215e00
Dan6erbond <moravrav@gmail.com>
2020-07-20 15:46:48 +0200

    Update metadata
    Author, centralized version, requirements and license updated to reflect new changes.

--- banhammer/__init__.py
+++ banhammer/__init__.py
@@ -1,11 +1,11 @@
-from .banhammer import Banhammer
-from .exceptions import *
-from .item import RedditItem
 from . import item as ItemHelper
-from .messagebuilder import MessageBuilder
-from .reaction import Reaction, ReactionPayload, ReactionHandler
 from . import reaction as ReactionHelper
 from . import reddithelper as RedditHelper
-from .subreddit import Subreddit
 from . import yaml as YAMLParser
-from . import config as Config
+from .banhammer import Banhammer
+from .const import __author__, __license__, __tag__, __version__
+from .exceptions import *
+from .item import RedditItem
+from .messagebuilder import MessageBuilder
+from .reaction import Reaction, ReactionHandler, ReactionPayload
+from .subreddit import Subreddit
