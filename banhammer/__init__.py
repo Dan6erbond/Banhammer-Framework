6128bfde596a27a84e19f91e76e386aa1f413faf
Dan6erbond <moravrav@gmail.com>
2020-07-22 08:40:28 +0200

    Optimize imports

--- banhammer/__init__.py
+++ banhammer/__init__.py
@@ -1,11 +1,2 @@
-from . import item as ItemHelper
-from . import reaction as ReactionHelper
-from . import reddit_helper as RedditHelper
-from . import yaml as YAMLParser
 from .banhammer import Banhammer
 from .const import __author__, __license__, __tag__, __version__
-from .exceptions import *
-from .item import RedditItem
-from .message_builder import MessageBuilder
-from .reaction import Reaction, ReactionHandler, ReactionPayload
-from .subreddit import Subreddit
--- banhammer/models/__init__.py
+++ banhammer/models/__init__.py
@@ -2,3 +2,4 @@ from .item import RedditItem
 from .message_builder import MessageBuilder
 from .reaction import (Reaction, ReactionHandler, ReactionPayload,
                        get_reactions, ignore_reactions)
+from .subreddit import Subreddit
